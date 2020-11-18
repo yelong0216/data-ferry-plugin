1d9689a035b1278f1a6462dbecbac69019843dcd
YL <yl1430834495@163.com>
2020-11-18 19:01:09 +0800

    发布1.0.1

--- src/main/java/dream/first/product/dataferry/plugin/configuration/DataFerryPluginRabbitMQConfiguration.java
+++ src/main/java/dream/first/product/dataferry/plugin/configuration/DataFerryPluginRabbitMQConfiguration.java
@@ -1,10 +1,9 @@
 package dream.first.product.dataferry.plugin.configuration;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.yelong.commons.util.ResourcesUtils;
+import org.springframework.core.env.Environment;
 import org.yelong.support.mq.rabbitmq.RabbitMQConnectionProperties;
-import org.yelong.support.yaml.YamlProperties;
-import org.yelong.support.yaml.YamlWrapper;
 
 import dream.first.product.dataferry.plugin.rabbitmq.DataFerryRabbitMQTool;
 import dream.first.product.dataferry.plugin.rabbitmq.DefaultDataFerryRabbitMQTool;
@@ -12,12 +11,14 @@ import dream.first.product.dataferry.plugin.rabbitmq.DefaultDataFerryRabbitMQToo
 public class DataFerryPluginRabbitMQConfiguration {
 
 	@Bean
-	public DataFerryRabbitMQTool dataFerryRabbitMQTool() {
-		YamlWrapper yamlWrapper = new YamlWrapper();
-		YamlProperties yamlProperties = yamlWrapper.load(ResourcesUtils.getResourceAsStream("dataferry.yml"));
-		String dataFerryQueueName = yamlProperties.getProperty("dataferry.rabbitmqQueueName");
-		RabbitMQConnectionProperties rabbitMQConnectionProperties = yamlProperties.as("rabbitmq",
-				RabbitMQConnectionProperties.class);
+	public DataFerryRabbitMQTool dataFerryRabbitMQTool(ApplicationContext applicationContext) {
+		Environment environment = applicationContext.getEnvironment();
+		String dataFerryQueueName = environment.getProperty("dataferry.rabbitmqQueueName");
+		RabbitMQConnectionProperties rabbitMQConnectionProperties = new RabbitMQConnectionProperties();
+		rabbitMQConnectionProperties.setHost(environment.getProperty("rabbitmq.host"));
+		rabbitMQConnectionProperties.setUsername(environment.getProperty("rabbitmq.username"));
+		rabbitMQConnectionProperties.setPassword(environment.getProperty("rabbitmq.password"));
+		rabbitMQConnectionProperties.setPort(environment.getProperty("rabbitmq.port", Integer.class));
 		DataFerryRabbitMQTool dataFerryRabbitMQTool = new DefaultDataFerryRabbitMQTool();
 		dataFerryRabbitMQTool.setDataFerryQueueName(dataFerryQueueName);
 		dataFerryRabbitMQTool.setRabbitMQConnectionProperties(rabbitMQConnectionProperties);
